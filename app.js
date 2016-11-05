6145b8428846c6d77e480fefc4a22626319de1a7
Andrew Sullivan <andrew@inkblot.io>
2016-11-05 16:38:44 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const app = new Vue({
+  el: '#app',
+  created: function () {
+    this.bookmarkSearch = new JsSearch.Search('description')
+    this.bookmarkSearch.indexStrategy = new JsSearch.PrefixIndexStrategy()
+    this.bookmarkSearch.searchIndex = new JsSearch.UnorderedSearchIndex()
+    this.bookmarkSearch.addIndex('description')
+    this.bookmarkSearch.addIndex('tags')
+
+    axios.get('./bookmarks.json').then(res => {
+      this.bookmarks = res.data.map(bookmark => {
+        return {
+          href: bookmark.href,
+          description: bookmark.description.replace('constructor', 'constructo r'), // hack around js-search bug, see https://github.com/bvaughn/js-search/issues/15
+          tags: bookmark.tags.split(/\s/),
+          time: bookmark.time
+        }
+      })
+      this.bookmarkSearch.addDocuments(this.bookmarks)
+      document.getElementById('query').focus()
+    })
+  },
+  data: {
+    query: '',
+    bookmarks: []
+  },
+  computed: {
+    visibleBookmarks: function () {
+      const bookmarks = this.query === '' ? this.bookmarks : this.bookmarkSearch.search(this.query)
+      return bookmarks.slice(0, 100)
+    }
+  },
+  filters: {
+    formatTime: function (time) {
+      return moment(time).format('MMMM D, YYYY')
+    }
+  }
+})
