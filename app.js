1178540ddf1110b7b4276fdb77b58b1cbc5ce9dd
Andrew Sullivan <andrew@inkblot.io>
2016-11-19 16:32:07 -0800

    Add vue-router, refactor

--- app.js
+++ app.js
@@ -1,36 +1,25 @@
-const app = new Vue({
-  el: '#app',
-  created: function () {
-    this.bookmarkSearch = new JsSearch.Search('description')
-    this.bookmarkSearch.indexStrategy = new JsSearch.PrefixIndexStrategy()
-    this.bookmarkSearch.searchIndex = new JsSearch.UnorderedSearchIndex()
-    this.bookmarkSearch.addIndex('description')
-    this.bookmarkSearch.addIndex('tags')
-
-    axios.get('./bookmarks.json').then(res => {
-      this.bookmarks = res.data.map(bookmark => {
-        return {
-          href: bookmark.href,
-          description: bookmark.description,
-          tags: bookmark.tags.split(/\s/),
-          time: bookmark.time
-        }
-      })
-      this.bookmarkSearch.addDocuments(this.bookmarks)
-      document.getElementById('query').focus()
-    })
+const SearchView = {
+  template: '#search-view',
+  props: ['bookmarks', 'search'],
+  mounted: function () {
+    document.getElementById('query').focus()
   },
-  data: {
-    query: '',
-    bookmarks: []
+  data: function () {
+    return {
+      query: ''
+    }
   },
   computed: {
     visibleBookmarks: function () {
-      const bookmarks = this.query === '' ? this.bookmarks : this.bookmarkSearch.search(this.query)
+      const bookmarks = this.query === '' ? this.bookmarks : this.search.search(this.query)
       return bookmarks.slice(0, 100)
     }
-  },
-})
+  }
+}
+
+const AddView = {
+  template: '#add-view'
+}
 
 const bookmark = Vue.component('bookmark', {
   template: '#bookmark-template',
@@ -41,3 +30,36 @@ const bookmark = Vue.component('bookmark', {
     }
   }
 })
+
+const routes = [
+  { path: '/', component: SearchView },
+  { path: '/add', component: AddView }
+]
+const router = new VueRouter({ routes })
+const app = new Vue({
+  created: function () {
+    this.search = new JsSearch.Search('description')
+    this.search.indexStrategy = new JsSearch.PrefixIndexStrategy()
+    this.search.searchIndex = new JsSearch.UnorderedSearchIndex()
+    this.search.addIndex('description')
+    this.search.addIndex('tags')
+
+    axios.get('./bookmarks.json').then(res => {
+      this.bookmarks = res.data.map(bookmark => {
+        return {
+          href: bookmark.href,
+          description: bookmark.description,
+          tags: bookmark.tags.split(/\s/),
+          time: bookmark.time
+        }
+      })
+      this.search.addDocuments(this.bookmarks)
+    })
+  },
+  data: {
+    bookmarks: [],
+    search: null
+  },
+  router
+}).$mount('#app')
+
